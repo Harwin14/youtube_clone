00aae8e659f12c86c47447c7b8ddda9e08d04f45
Harwin14 <agungharwinn@gmail.com>
2023-06-26 18:37:12 +0700

    r

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,58 +4,84 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail, marginTop }) => (
-    <Box sx={{ 
-        boxShadow: "none", 
-        borderRadius: "20px",
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        width:{xs:'356px', md:'315px'},
-        height:'326px',
-        margin:'auto',
-        marginTop
-        }}>
-        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-            <CardContent
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    textAlign: "center",
-                    color: "#fff",
-                }}
-            >
-                <CardMedia
-                    image={
-                        channelDetail?.snippet?.thumbnails?.high?.url ||
-                        demoProfilePicture
-                    }
-                    alt={channelDetail?.snippet?.title}
+const ChannelCard = ({ channelDetail, marginTop }) => {
+    return (
+        <Box
+            sx={{
+                boxShadow: "none",
+                borderRadius: "20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: { xs: "356px", md: "315px" },
+                height: "326px",
+                margin: "auto",
+                marginTop,
+            }}
+        >
+            <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+                <CardContent
                     sx={{
-                        borderRadius: "50%",
-                        height: "180px",
-                        width: "180px",
-                        mb: 2,
-                        border: "1px solid #e3e3e3",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        textAlign: "center",
+                        color: "#fff",
                     }}
-                />
-                <Typography variant="h6">
-                    {channelDetail?.snippet?.title.slice(0,19)}
-                    <CheckCircle
-                        sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+                >
+                    <CardMedia
+                        image={
+                            channelDetail?.snippet?.thumbnails?.high?.url ||
+                            demoProfilePicture
+                        }
+                        alt={channelDetail?.snippet?.title}
+                        sx={{
+                            borderRadius: "50%",
+                            height: "180px",
+                            width: "180px",
+                            mb: 2,
+                            border: "1px solid #e3e3e3",
+                        }}
                     />
-                </Typography>
-                {channelDetail?.statistics?.subscriberCount && (
-                    <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
-                        {parseInt(
-                            channelDetail?.statistics?.subscriberCount
-                        ).toLocaleString('en-US')} Subscribers
+                    <Typography variant="h6">
+                        {channelDetail?.snippet?.title.slice(0, 19)}
+                        <CheckCircle
+                            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+                        />
                     </Typography>
-                )}
-            </CardContent>
-        </Link>
-    </Box>
-);
+                    {channelDetail?.statistics?.subscriberCount && (
+                        <>
+                        
+                        <Typography
+                            sx={{
+                                fontSize: "15px",
+                                fontWeight: 500,
+                                color: "gray",
+                            }}
+                        >
+                            {parseInt(
+                                channelDetail?.statistics?.subscriberCount
+                            ).toLocaleString("en-US")}{" "}
+                            Subscribers
+                        </Typography>
+                                <Typography
+                                sx={{
+                                    fontSize: "15px",
+                                    fontWeight: 500,
+                                    color: "gray",
+                                }}
+                            >
+                                {parseInt(
+                                    channelDetail?.statistics?.videoCount
+                                ).toLocaleString("en-US")}{" "}
+                                Videos
+                            </Typography>
+                            </>
+                    )}
+                </CardContent>
+            </Link>
+        </Box>
+    );
+};
 
 export default ChannelCard;
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,10 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, CardMedia, Toolbar, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPi";
-import { bgkaela } from "../utils/constants";
-import bgkaela from '../utils/bg-kaela.png'
+import { demoProfilePicture } from "../utils/constants";
 
 const ChannelDetail = () => {
     const [channelDetail, setChannelDetail] = useState(null);
@@ -24,20 +23,65 @@ const ChannelDetail = () => {
     return (
         <Box minHeight="95vh">
             <Box>
-                <div
-                    style={{
-                        backgroundImage: bgkaela,
-                        zIndex: 10,
+                <CardMedia
+                    component="img"
+                    src={
+                        "  https://yt3.googleusercontent.com/DyQFbaS4xwZTB9Au3OFa4VcJebFO_o2nf0apiq8B4h6xRnC2FPkzIMkAREu2XUdjQC8oiR8S=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+                    }
+                    alt={channelDetail?.snippet?.title}
+                    sx={{
                         height: "300px",
+                        objectFit: "cover",
                     }}
-
-                 /> 
+                />
                 <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+
+                <AppBar
+                    position="static"
+                    style={{
+                        backgroundColor: "transparent",
+                        boxShadow: "none",
+                        display:'flex',
+                        justifyContent:'center',
+                        borderBottom: "1px solid #vvv",
+                        marginBottom:'20px'
+                    }}
+                >
+                    <Toolbar    style={{
+                        alignItems:'center',
+                        justifyContent:'center',
+                        gap:20
+                    }}>
+                        <Typography
+                            variant="h6"
+                            style={{ marginRight: "10px", }}
+                        >
+                            <a href="#" style={{ color: "#fff" ,padding:'2px 10px' }}>Beranda</a>
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            style={{ marginRight: "10px", background: "#bbb",padding:'2px 10px' , borderRadius:'5px'  }}
+                        >
+                            <a href="#" style={{ color: "#000" }}>Video</a>
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            style={{ marginRight: "10px", color: "#fff",padding:'2px 10px' }}
+                        >
+                            <a href="#" style={{ color: "#fff" }}>Shorts</a>
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            style={{ marginRight: "10px", color: "#fff" }}
+                        >
+                            <a href="#" style={{ color: "#fff",padding:'2px 10px' }}>Live</a>
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
             </Box>
             <Box display="flex" p="2">
                 <Box sx={{ mr: { sm: "100px" } }} />
-                  <Videos videos={videos}/>
-           
+                <Videos videos={videos} />
             </Box>
         </Box>
     );
